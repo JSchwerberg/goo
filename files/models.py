114600fa2e8de169e8448f620fafed797917357f
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2014-06-15 01:06:15 +0000

    First Commit

---
+++ files/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class SearchIndex(models.Model):
+    filename = models.CharField(max_length=250)
+    path = models.CharField(max_length=350)
+    folder = models.CharField(max_length=350)
+    md5 = models.CharField(max_length=32)
+    type = models.CharField(max_length=8)
+    description = models.TextField()
+    is_flashable = models.BooleanField(default=False)
+    modified = models.CharField(max_length=35)
+    downloads = models.PositiveIntegerField(default=0)
+    status = models.SmallIntegerField(default=1)
+    additional_info = models.TextField()
+    short_url = models.CharField(max_length=32)
+    developer_id = models.IntegerField(default=5)
+    ro_developerid = models.CharField(max_length=50)
+    ro_board = models.CharField(max_length=50)
+    ro_rom = models.CharField(max_length=65)
+    ro_version = models.IntegerField()
+    gapps_package = models.IntegerField()
+    incremental_file = models.IntegerField()
+    filesize = models.BigIntegerField()
+
+class Blacklist(models.Model):
+    keyword = models.CharField(max_length=150)
+    status = models.BooleanField()
+
+
---
+++ goo_new/urls/api.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns =  patterns('',
+    url(r'^oauth2/' include('provider.oauth2.urls', namespace = 'oauth2')),
+}
