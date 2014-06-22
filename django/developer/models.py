eaaef66f430055fc58af6c2f969a0edca0dbb515
Junaos <Junaos@gmail.com>
2014-06-22 13:04:18 -0700

    Started authentication backend for sponsors

--- django/developer/models.py
+++ django/developer/models.py
@@ -3,7 +3,6 @@ from django.utils.translation import ugettext as _
 
 # Create your models here.
 
-<<<<<<< HEAD
 class Application(models.Model):
 	alias = models.CharField(max_length=150)
 	releases = models.TextField()
@@ -21,12 +20,10 @@ class Application(models.Model):
 	def __unicode__(self):
 		return u'%s' % self.alias
 
-=======
->>>>>>> b0d6919c9533096f0b05e82bda721e24c109f6d5
+
 class Developer(models.Model):
 	developer_path = models.CharField(max_length=450)
 	username = models.CharField(max_length=100)
-	password = models.CharField(max_length=200)
 	avatar = models.CharField(max_length=250)
 	avatar_thumb = models.CharField(max_length=50)
 	bio = models.TextField()
@@ -37,50 +34,12 @@ class Developer(models.Model):
 	email = models.CharField(max_length=250)
 	xda_rd_url = models.CharField(max_length=50)
 	created = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
 	status = models.BooleanField(default=False)
 	application = models.OneToOneField(Application)
 
-
-=======
-	status = models.BooleanField()
-	denied = models.BooleanField()
-    
->>>>>>> b0d6919c9533096f0b05e82bda721e24c109f6d5
 	class Meta:
 		verbose_name = _('Developer')
         verbose_name_plural = _('Developers')
 
 	def __unicode__(self):
 		return u'%s' % self.username
-
-<<<<<<< HEAD
-=======
-
-class Application(models.Model):
-	alias = models.CharField(max_length=150)
-	email = models.CharField(max_length=150)
-	twitter = models.CharField(max_length=250)
-	googleplus = models.CharField(max_length=350)
-	xda = models.CharField(max_length=350)
-	bio = models.TextField()
-	releases = models.TextField()
-	username = models.CharField(max_length=250)
-	foldername = models.CharField(max_length=250)
-	services = models.CharField(max_length=250)
-	referral = models.CharField(max_length=250)
-	timestamp = models.DateTimeField(auto_now_add=True)
-	processed = models.BooleanField()
-	denied = models.BooleanField()
-
-	class Meta:
-		verbose_name = _('Application')
-		verbose_name_plural = _('Applications')
-
-	def __unicode__(self):
-		return u'%s' % self.alias
-
-
->>>>>>> b0d6919c9533096f0b05e82bda721e24c109f6d5
-    
-            
