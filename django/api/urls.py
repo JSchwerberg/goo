5cb36887e088c943fa4674241fbfb566edaedc02
Junaos <Junaos@gmail.com>
2014-06-25 16:03:23 -0700

    API

--- django/api/urls.py
+++ django/api/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import patterns. url
 from api import views
 
 urlpatterns = patterns('',
-	url(r'^files/', views.file_list, name='file_list'),
-	
+	url(r'^files/', 'api.views.file_list', name='file_list'),
 	)
