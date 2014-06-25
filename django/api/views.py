26d349cf6a0d4d67ed6af22131b84082f88a1beb
Junaos <Junaos@gmail.com>
2014-06-25 15:59:06 -0700

    Refactored API, exposed URLs

--- django/api/views.py
+++ django/api/views.py
@@ -7,7 +7,7 @@ from files.models import File
 from files.serializers import PaginatedFileSerializer, FileSerializer
 
 
-@api_view('GET')
+@api_view('GET', 'POST')
 def file_list(request):
     """
     List all files in the file index, paginated.  Supports an optional 
@@ -16,57 +16,53 @@ def file_list(request):
     how many items per page, and a 'page' parameter to get a specific
     page number from the results.
     """
+    if request.method == 'GET':
+        # We only want to show items that are 'active'
+        queryset = File.objects.all().filter(status=1)
 
-    # We only want to show items that are 'active'
-    queryset = File.objects.all().filter(status=1)
+        # Set optional search terms
+        search_terms = request.QUERY_PARAMS.get('search')
+        if search_terms not None:
+        	# Equivalent to "SELECT * from files WHERE MATCH(files, filename)  
+        	# AGAINST (search_terms IN BOOLEAN MODE) OR MATCH(files, description) 
+        	# AGAINST (search_terms IN BOOLEAN MODE)"
+        	queryset = queryset.filter(Q(filename__search=search_terms) | 
+        		Q(description__search=search_terms))
 
-    # Set optional search terms
-    search_terms = request.QUERY_PARAMS.get('search')
-    if search_terms not None:
-    	# Equivalent to "SELECT * from files WHERE MATCH(files, filename)  
-    	# AGAINST (search_terms IN BOOLEAN MODE) OR MATCH(files, description) 
-    	# AGAINST (search_terms IN BOOLEAN MODE)"
-    	queryset = queryset.filter(Q(filename__search=search_terms) | 
-    		Q(description__search=search_terms))
+        # Sort queryset by modified date (descending),  
+        # removing any duplicate entries
+        queryset = queryset.order_by('-modified').distinct('id')
+        
+        # Allow API end-users to specify a custom amount of items per page
+        items_per_page = request.QUERY_PARAMS.get('items') 
+        if not items_per_page or not isinstance(items_per_page, int):
+        	items_per_page = 10
 
-    # Sort queryset by modified date (descending),  
-    # removing any duplicate entries
-    queryset = queryset.order_by('-modified').distinct('id')
+    	paginator = Paginator(queryset, items_per_page)
+        
+        page = request.QUERY_PARAMS.get('page')
+        try:
+        	files = paginator.page(page)
+        except PageNotAnInteger:
+        	# If page is not an integer, deliver first page
+        	files = paginator.page(1)
+        except EmptyPage:
+        	# If page is out of range
+        	# deliver last page of results
+        	files = paginator.page(paginator.num_pages)
+
+        serializer_context = {'request': request}
+        serializer = PaginatedFileSerializer(files, context=serializer_context)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.method == 'POST':
+        serializer = FileSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Repsonse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    # Allow API end-users to specify a custom amount of items per page
-    items_per_page = request.QUERY_PARAMS.get('items') 
-    if not items_per_page or not isinstance(items_per_page, int):
-    	items_per_page = 10
-
-	paginator = Paginator(queryset, items_per_page)
-    
-    page = request.QUERY_PARAMS.get('page')
-    try:
-    	files = paginator.page(page)
-    except PageNotAnInteger:
-    	# If page is not an integer, deliver first page
-    	files = paginator.page(1)
-    except EmptyPage:
-    	# If page is out of range
-    	# deliver last page of results
-    	files = paginator.page(paginator.num_pages)
-
-    serializer_context = {'request': request}
-    serializer = PaginatedFileSerializer(files, context=serializer_context)
-
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-@api_view('POST')
-def file_create(request):
-    """
-    Create a new file entry in the index.
-    """
-
-    serializer = FileSerializer(data=request.DATA)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Repsonse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def file_detail(request, pk):
