1c63648cd2f8df5b06be5f5d7cdb8e1050ab6cd5
Junaos <Junaos@gmail.com>
2014-06-25 16:05:08 -0700

    API

--- django/goo_new/urls/api.py
+++ django/goo_new/urls/api.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns =  patterns('',
     # url(r'^oauth2/' include('provider.oauth2.urls', namespace = 'oauth2')),
-    url(r'^$,', include('api.urls', namespace="api")),
+    url(r'^$', include('api.urls', namespace="api")),
 )
