21bfe7b1e3bdcae07cc0d59076f81f9bcc39cbcb
Junaos <Junaos@gmail.com>
2014-06-25 17:55:06 -0700

    Push

--- goo_new/api/serializers.py
+++ goo_new/api/serializers.py
@@ -9,7 +9,7 @@ class FileSerializer(serializers.ModelSerializer):
         
         # Only these fields should be exposed by the API /at all/
         fields = ('id', 'filename', 'path', 'folder', 'md5', 'filetype', 'description', 
-        	'is_flashable', 'incremental_file', 'modified', 'ro_developerid', 'ro_board',  
+        	'is_flashable', 'incremental_file', 'modified', 'developer_id', 'ro_developerid', 'ro_board',  
         	'ro_rom', 'ro_version', 'gapps_package', 'filesize', 
         	'download_count', 'last_download' )
 
