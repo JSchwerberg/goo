c58d44b0a68ca41c7b212ba562c3d26d728bd5d0
Junaos <Junaos@gmail.com>
2014-06-28 11:18:15 -0700

    Push

--- goo_new/blog/views.py
+++ goo_new/blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 
 class IndexView(TemplateView):
-	template_name = "index.html"
+	template_name = "base_home.html"
 
 	def get_context_data(self, **kwargs):
 		context = super(IndexView, self).get_context_data(**kwargs)
