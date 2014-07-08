9242ff7150d2d85d96481f22769fac01b880944c
Junaos <Junaos@junaos.com>
2014-07-08 15:05:32 -0700

    Message

--- goo_new/goo_new/settings/production.py
+++ goo_new/goo_new/settings/production.py
@@ -21,7 +21,7 @@ DATABASES = {
     }
 }
 
-PAYPAL_RECEIVER_EMAIL = "basnipa@gmail.com"
+PAYPAL_RECEIVER_EMAIL = "webmaster@junaos.com"
 
 API_TOKEN = 'nQPfNiMESUdw2SfbRVUNc07TN7UvFsNn'
 
@@ -72,3 +72,5 @@ GAPPS_KEY = "93heq298unrf312890h"
 
 CSRF_COOKIE_DOMAIN = ".goo.im"
 CSRF_COOKIE_SECURE = True
+
+PAYPAL_TEST=True
