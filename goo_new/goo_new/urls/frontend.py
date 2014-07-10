87413c3ff5c37af309a4586e6b9f8e21b4022518
Junaos <junaos@junaos.com>
2014-07-09 21:16:05 -0700

    Pushj

--- goo_new/goo_new/urls/frontend.py
+++ goo_new/goo_new/urls/frontend.py
@@ -1,3 +1,4 @@
+from django.views.generic import RedirectView
 from django.conf.urls import patterns, include, url
 from blog.views import IndexView
 from files.views import gapps_list, gapps_download
@@ -8,7 +9,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'goo_new.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^confirmation', RedirectView.as_view(url=reverse_lazy('index'), permanent=False)),
     url(r'^sponsor/', include('sponsor.urls')),    
     url(r'^devs/', include('files.urls')),
     url(r'^admin/', include(admin.site.urls)),
