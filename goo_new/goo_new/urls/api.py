e8759081b9f73fc949dce552322f66e0f046690c
Junaos <Junaos@junaos.com>
2014-08-12 13:24:47 -0700

    Moved delete function to admin area of API

--- goo_new/goo_new/urls/api.py
+++ goo_new/goo_new/urls/api.py
@@ -11,11 +11,12 @@ urlpatterns =  patterns('',
     url(r'^files/devs$', 'api.views.folder_list'),
     url(r'^files/devs/$', 'api.views.folder_list'), 
     url(r'^files/dev/(?P<dev>.*?)$', 'api.views.developer_file_list', name='dev_file_list'),
-    url(r'^files/(?P<pk>\d+)/$', 'api.views.file_detail', name='file_detail'),
+    # url(r'^files/(?P<pk>\d+)/$', 'api.views.file_detail', name='file_detail'),
     url(r'^files/(?P<folder>.*?)$', 'api.views.file_list', name='file_list'),
     # url(r'^files/', 'api.views.file_list', name='file_list'),
     url(r'^install_command/(?P<device>.*?)/$', 'api.views.install_command_view', name='install_command'),
     url(r'^admin/devs/(?P<path>.*?)/$', 'api.views.developer_info', name='developer_info'),
     url(r'^admin/ics/$', 'api.views.latest_ics', name='latest_ics'),
     url(r'^admin/jb/$', 'api.views.latest_jb', name='latest_jb')
+    url(r'^admin/delete/(?P<path>.*?)/$', 'api.views.file_delete', name='file_delete')
 )
