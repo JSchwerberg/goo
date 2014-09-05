7ed3a5e88359f5073786df8069c4d19a48648c36
Junaos <Junaos@junaos.com>
2014-09-05 10:18:13 -0700

    TWRP Adblock Code

--- goo_new/fabfile.py
+++ goo_new/fabfile.py
@@ -5,13 +5,18 @@ from fabric.contrib import project
 
 # RSync Settings
 
-env.hosts = ['junaos.com']
-env.key_file = ['/home/django/.ssh/id_rsa']
-env.user = 'goostatic'
-env.local_static_root = '/home/django/goo/goo_new/static/'
-env.remote_static_root = '/var/www/goostatic'
+env.roledefs = {
+    'web': ['django2.goo.im'],
+    'static': ['junaos.com']
+}
+
+env.keyfile = '/home/django/.ssh/id_rsa'
 
 def deploy_static():
+    env.hosts = ['junaos.com']
+    env.user = 'goostatic'
+    env.local_static_root = '/home/django/goo/goo_new/static/'
+    env.remote_static_root = '/var/www/goostatic'
     local('python manage.py collectstatic --settings=goo_new.settings.production')
     project.rsync_project(
         remote_dir = env.remote_static_root,
@@ -20,3 +25,13 @@ def deploy_static():
     )
 
 
+def deploy_server():
+    env.hosts = ['django2.goo.im']
+    env.user = 'django'
+    env.local_static_root = '/home/django/goo/goo_new/'
+    env.remote_static_root = '/home/django/goo/goo_new/'
+    project.rsync_project(
+        remote_dir = env.remote_static_root,
+        local_dir = env.local_static_root,
+        delete = True
+    )
